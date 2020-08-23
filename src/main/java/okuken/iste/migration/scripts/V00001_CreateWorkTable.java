b0beeaf5f90ebf14aa49fa6ebcbab851e928ab1c
Kenichi Okuno <o.kenichi.gm@gmail.com>
2020-08-23 23:26:05 +0900

    remove test migration scripts

--- src/main/java/okuken/iste/migration/scripts/V00001_CreateWorkTable.java
+++
@@ -1,8 +0,0 @@
-package okuken.iste.migration.scripts;
-
-public class V00001_CreateWorkTable implements PrefixedMigrationScript {
-	@Override
-	public String getUpScript() {
-		return "CREATE TABLE ISTE_WORK(ID INTEGER PRIMARY KEY AUTOINCREMENT, VALUE);";
-	}
-}
--- src/main/java/okuken/iste/migration/scripts/V00002_CreateWorkTable2.java
+++
@@ -1,8 +0,0 @@
-package okuken.iste.migration.scripts;
-
-public class V00002_CreateWorkTable2 implements PrefixedMigrationScript {
-	@Override
-	public String getUpScript() {
-		return "CREATE TABLE ISTE_WORK2(ID INTEGER PRIMARY KEY AUTOINCREMENT, VALUE);";
-	}
-}
