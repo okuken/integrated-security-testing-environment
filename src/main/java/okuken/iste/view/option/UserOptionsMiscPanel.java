21ed8cbd1febcb443104f3bff132315abfdfffe5
Kenichi Okuno <o.kenichi.gm@gmail.com>
2022-02-18 00:48:02 +0900

    fix to follow font size changes

--- src/main/java/okuken/iste/view/option/UserOptionsMiscPanel.java
+++ src/main/java/okuken/iste/view/option/UserOptionsMiscPanel.java
@@ -19,6 +19,9 @@ import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class UserOptionsMiscPanel extends JPanel {
 
@@ -36,37 +39,11 @@ public class UserOptionsMiscPanel extends JPanel {
 	private JCheckBox useKeyboardShortcutWithClickCheckBox;
 
 	public UserOptionsMiscPanel() {
-		setLayout(null);
-		
-//		JLabel userNameLabel = new JLabel(Captions.USER_OPTIONS_USER_NAME + ":");
-//		userNameLabel.setFont(new Font("MS UI Gothic", Font.PLAIN, 12));
-//		userNameLabel.setBounds(30, 60, 100, 30);
-//		add(userNameLabel);
-//		
-//		userNameTextField = new JTextField();
-//		userNameTextField.setBounds(140, 60, 210, 30);
-//		add(userNameTextField);
-//		userNameTextField.setColumns(20);
-//		userNameTextField.setText(ConfigLogic.getInstance().getUserOptions().getUserName());
-//		
-//		JButton userNameSaveButton = new JButton("Save");
-//		userNameSaveButton.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				//TODO: validation
-//				ConfigLogic.getInstance().saveUserName(userNameTextField.getText());
-//			}
-//		});
-//		userNameSaveButton.setBounds(400, 60, 120, 30);
-//		add(userNameSaveButton);
 		
 		JLabel dbFileLabel = new JLabel(Captions.USER_OPTIONS_DB_FILE_PATH + ":");
-		dbFileLabel.setBounds(30, 25, 100, 30);
-		add(dbFileLabel);
 		
 		dbFileTextField = new JTextField();
-		dbFileTextField.setColumns(20);
-		dbFileTextField.setBounds(170, 25, 300, 30);
-		add(dbFileTextField);
+		dbFileTextField.setColumns(30);
 		
 		JButton dbFileChooseButton = new JButton(Captions.FILECHOOSER);
 		dbFileChooseButton.addActionListener(new ActionListener() {
@@ -77,8 +54,6 @@ public class UserOptionsMiscPanel extends JPanel {
 				}
 			}
 		});
-		dbFileChooseButton.setBounds(470, 25, 20, 30);
-		add(dbFileChooseButton);
 		
 		JButton dbFileSaveButton = new JButton(Captions.USER_OPTIONS_DB_FILE_BUTTON_SAVE);
 		dbFileSaveButton.addActionListener(new ActionListener() {
@@ -88,16 +63,10 @@ public class UserOptionsMiscPanel extends JPanel {
 				UiUtil.showTemporaryMessage(dbFileMessageLabel, Captions.MESSAGE_DONE);
 			}
 		});
-		dbFileSaveButton.setBounds(500, 25, 120, 30);
-		add(dbFileSaveButton);
 		
 		dbFileMessageLabel = UiUtil.createTemporaryMessageArea();
-		dbFileMessageLabel.setBounds(570, 25, 200, 30);
-		add(dbFileMessageLabel);
 		
 		JLabel themeLabel = new JLabel(Captions.USER_OPTIONS_THEME + ":");
-		themeLabel.setBounds(30, 75, 45, 30);
-		add(themeLabel);
 		
 		themeComboBox = new JComboBox<String>();
 		themeComboBox.addActionListener(new ActionListener() {
@@ -106,16 +75,10 @@ public class UserOptionsMiscPanel extends JPanel {
 			}
 		});
 		themeComboBox.setModel(new DefaultComboBoxModel<String>(new String[] {THEME_LIGHT, THEME_DARK}));
-		themeComboBox.setBounds(170, 75, 80, 30);
-		add(themeComboBox);
 		
 		JLabel themeExplanationLabel = new JLabel(Captions.USER_OPTIONS_THEME_EXPLANATION);
-		themeExplanationLabel.setBounds(260, 75, 700, 30);
-		add(themeExplanationLabel);
 		
 		JLabel useKeyboardShortcutLabel = new JLabel(Captions.USER_OPTIONS_USE_KEYBOARD_SHORTCUT + ":");
-		useKeyboardShortcutLabel.setBounds(30, 130, 135, 13);
-		add(useKeyboardShortcutLabel);
 		
 		useKeyboardShortcutQCheckBox = new JCheckBox(Captions.USER_OPTIONS_USE_KEYBOARD_SHORTCUT_Q);
 		useKeyboardShortcutQCheckBox.addActionListener(new ActionListener() {
@@ -126,8 +89,6 @@ public class UserOptionsMiscPanel extends JPanel {
 				applyUseKeyboardShortcutQ(useKeyboardShortcutQ);
 			}
 		});
-		useKeyboardShortcutQCheckBox.setBounds(170, 126, 600, 21);
-		add(useKeyboardShortcutQCheckBox);
 		
 		useKeyboardShortcutWithClickCheckBox = new JCheckBox(Captions.USER_OPTIONS_USE_KEYBOARD_SHORTCUT_WITH_CLICK);
 		useKeyboardShortcutWithClickCheckBox.addActionListener(new ActionListener() {
@@ -135,8 +96,58 @@ public class UserOptionsMiscPanel extends JPanel {
 				ConfigLogic.getInstance().saveUseKeyboardShortcutWithClick(useKeyboardShortcutWithClickCheckBox.isSelected());
 			}
 		});
-		useKeyboardShortcutWithClickCheckBox.setBounds(170, 150, 450, 21);
-		add(useKeyboardShortcutWithClickCheckBox);
+
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(useKeyboardShortcutLabel)
+						.addComponent(dbFileLabel)
+						.addComponent(themeLabel))
+					.addGap(30)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(dbFileTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addComponent(dbFileChooseButton)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(dbFileSaveButton)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(dbFileMessageLabel))
+						.addComponent(useKeyboardShortcutQCheckBox)
+						.addComponent(useKeyboardShortcutWithClickCheckBox)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(themeComboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(themeExplanationLabel)))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(dbFileLabel)
+						.addComponent(dbFileTextField)
+						.addComponent(dbFileChooseButton)
+						.addComponent(dbFileSaveButton)
+						.addComponent(dbFileMessageLabel))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(themeLabel)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(themeComboBox)
+							.addComponent(themeExplanationLabel)))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createSequentialGroup()
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(useKeyboardShortcutLabel)
+							.addComponent(useKeyboardShortcutQCheckBox))
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(useKeyboardShortcutWithClickCheckBox)))
+		);
+		setLayout(groupLayout);
 
 		Controller.getInstance().setUserOptionsMiscPanel(this);
 		refresh(true);
